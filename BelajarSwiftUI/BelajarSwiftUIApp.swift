22e4fa3eec7e19df73699ffa81eb3715c05a7fe2
Muhammad Darda Fathammubienaa <muhammaddarda733@gmail.com>
2024-04-16 11:38:47 +0700

    Initial Commit

---
+++ BelajarSwiftUI/BelajarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BelajarSwiftUIApp.swift
+//  BelajarSwiftUI
+//
+//  Created by Muhammad Darda Fathammubienaa on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BelajarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
